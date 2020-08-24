61ee46192af2de9ee4c1543a4568f8a3f48e7a3c
CXTretar <misscxuan@163.com>
2020-08-24 11:21:37 +0800

    自定义首页本地导航组件。

--- flutter_trip/lib/pages/home/home_page.dart
+++ flutter_trip/lib/pages/home/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:flutter_trip/dao/home_dao.dart';
+import 'package:flutter_trip/model/common_model.dart';
 import 'package:flutter_trip/model/home_model.dart';
+import 'package:flutter_trip/widgets/local_nav.dart';
 
 const APPBAR_SCROLL_OFFSET = 100;
 
@@ -11,6 +13,8 @@ class CXHomePage extends StatefulWidget {
 }
 
 class _CXHomePageState extends State<CXHomePage> {
+  List<CommonModel> localNavList;
+
   @override
   void initState() {
     // TODO: implement initState
@@ -19,7 +23,14 @@ class _CXHomePageState extends State<CXHomePage> {
   }
 
   void _loadData() async {
-    HomeModel model = await HomeDao.fetch();
+    try {
+      HomeModel model = await HomeDao.fetch();
+      setState(() {
+        localNavList = model.localNavList;
+      });
+    } catch (e) {
+      print(e);
+    }
   }
 
   final _imageUrls = [
@@ -46,6 +57,7 @@ class _CXHomePageState extends State<CXHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xfff2f2f2),
       body: Stack(
         children: <Widget>[
           MediaQuery.removePadding(
@@ -73,7 +85,13 @@ class _CXHomePageState extends State<CXHomePage> {
                       },
                       pagination: SwiperPagination(),
                     ),
-                  )
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
+                    child: LocalNavWidget(
+                      localNavList: localNavList,
+                    ),
+                  ),
                 ],
               ),
             ),
---
+++ flutter_trip/lib/widgets/local_nav.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_trip/model/common_model.dart';
+
+class LocalNavWidget extends StatelessWidget {
+  final List<CommonModel> localNavList;
+
+  const LocalNavWidget({Key key, @required this.localNavList}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 64,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(7),
+        child: _items(context),
+      ),
+    );
+  }
+
+  Widget _items(BuildContext context) {
+    if (localNavList == null) return null;
+    List<Widget> items = [];
+    localNavList.forEach((model) {
+      Widget item = _item(context, model);
+      items.add(item);
+    });
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: items,
+    );
+  }
+
+  Widget _item(BuildContext context, CommonModel model) {
+    return GestureDetector(
+      onTap: () {},
+      child: Column(
+        children: <Widget>[
+          Image.network(
+            model.icon,
+            width: 32,
+            height: 32,
+          ),
+          Text(
+            model.title,
+            style: TextStyle(fontSize: 12),
+          ),
+        ],
+      ),
+    );
+  }
+}
